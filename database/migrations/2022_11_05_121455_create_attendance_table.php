77b2c5be6fbd9052c4e57055dadf3cc038576ddd
fsvhino <ferrysafrizal20@gmail.com>
2022-11-05 20:33:31 +0800

    BC-520 [Task][Attendance][FG][DB] Create Table/Migration 'attendance'

---
+++ database/migrations/2022_11_05_121455_create_attendance_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->id();
+            $table->longText('event')->nullable();
+            $table->string('name', 100);
+            $table->longText('address')->nullable();
+            $table->string('whatsapp', 15);
+            $table->string('instagram', 100)->nullable();
+            $table->string('institution', 150)->nullable();
+            $table->integer('time_visit')->nullable();
+            $table->longText('reason')->nullable();
+            $table->longText('info')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance');
+    }
+}
