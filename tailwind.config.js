ad622a4ec52cb65031763853a79c48f30f5b8d2d
asharinovaldy <asharinovaldi6319@gmail.com>
2023-02-15 17:46:18 +0800

    [Malahing] Malahing Buku Tamu - install tailwind form - revamp form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("@tailwindcss/forms")],
+};
